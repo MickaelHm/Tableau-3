99c6c390cab687e122684aa7aaf90a3c3a4f3f72
MickaelHm <harmeau.mickael@gmail.com>
2018-12-18 12:03:12 +0100

    Exercice complet

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tableau_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] week = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Jeudi", "Samedi", "Dimanche" };
+            foreach (string day in week)
+            {
+                Console.WriteLine(day);
+            }
+            week[4] = "Vendredi";
+            foreach (string day in week)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
